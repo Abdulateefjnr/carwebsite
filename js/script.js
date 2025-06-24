091e93df00e565e3ce0c091864061dbddb7c7648
Abdulateefjnr <120284775+Abdulateefjnr@users.noreply.github.com>
2025-06-24 10:40:10 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+// NAVBAR //
+var navbar =document.getElementById("navbar");
+
+function showMenu() {
+    navbar.style.top = " 0px";
+}
+
+function hideMenu() {
+  navbar.style.top = " -450px";
+}
+
+function wclose() {
+  navbar.style.top = " -450px";
+}
+
