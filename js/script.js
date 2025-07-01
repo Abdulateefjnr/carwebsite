1affc33d3a68c55bb92a01894c138bde6ec1a614
Abdulateefjnr <120284775+Abdulateefjnr@users.noreply.github.com>
2025-07-01 15:26:40 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,17 +2,27 @@
 
 
 // NAVBAR //
-var navbar =document.getElementById("navbar");
-
-function showMenu() {
-    navbar.style.top = " 0px";
-}
+let navbar = document.getElementById("navbar");
 
 function hideMenu() {
-  navbar.style.top = " -450px";
+    navbar.style.left = "-300%";
+}
+
+function showmenu() {
+    navbar.style.left = "5%";
 }
 
 function wclose() {
   navbar.style.top = " -450px";
 }
 
+// Animation on scroll function and init
+function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+}
+window.addEventListener('load', aosInit);
